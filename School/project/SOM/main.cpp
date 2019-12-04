ef7b757b9cfde303989ab870059f117bbe2b7a7c
lyz-win10 <qianmanage.lyz@gmail.com>
2019-12-04 21:58:52 +0800

    刪除 Unit 中不必要的 delta_w變數與相關方法、修正 'set_output()' 處的演算法理解錯誤

--- School/project/SOM/main.cpp
+++ School/project/SOM/main.cpp
@@ -18,7 +18,6 @@ class Unit {
         if (num > 0) {
             output = 0;
             weight.assign(num, 0.0);
-            delta_w.assign(num, 0.0);
 
             // 取隨機數的工具
             random_device rd;
@@ -32,10 +31,6 @@ class Unit {
         } else {
             cerr << "number must greater than 0\n";
         }
-    }
-
-    vector<double> get_delta_w(void) {
-        return delta_w;
     }
 
     vector<double> get_weight(void) {
@@ -52,18 +47,12 @@ class Unit {
         }
     }
 
-    void set_delta_w(vector<double> vec) {
-        if (vec.size() == delta_w.size()) {
-            delta_w = vec;
-        }
-    }
-
     void set_output(double val) {
         output = val;
     }
 
   private:
-    vector<double> weight, delta_w;
+    vector<double> weight;
     double output;
 };
 
@@ -129,7 +118,7 @@ class Topology2D {
                 for (int row = 0; row < topo.size(); row++) {
                     for (int col = 0; col < topo[row].size(); col++) {
                         // 初始化當前單元輸出值爲 0.0
-                        topo[row][col].set_output(0.0);
+                        //topo[row][col].set_output(0.0);
                         vector<double> wt = topo[row][col].get_weight();
 
                         // 計算鄰近距離
@@ -149,30 +138,32 @@ class Topology2D {
                 //cout << "Winner is (" << winner[1] << "," << winner[0] << ") - Unit "
                     //<< winner[0] * topo.size() + winner[1] + 1 << endl;
 
-                // 令優勝單元輸出爲 1
-                topo[winner[0]][winner[1]].set_output(1.0);
+                // 令優勝單元輸出爲該群集
+                topo[winner[0]][winner[1]].set_output(y_train[index_data]);
 
                 // TODO: 若 delta_w 不需要，則刪除其相關程式
                 // 計算修正量 delta W 後，更新 Weight
                 for (int row = 0; row < topo.size(); row++) {
                     for (int col = 0; col < topo[row].size(); col++) {
                         vector<double> wt = topo[row][col].get_weight();
-                        vector<double> delta_wt = topo[row][col].get_delta_w();
 
-                        for (int i = 0; i < delta_wt.size(); i++) {
+                        for (int i = 0; i < wt.size(); i++) {
                             // 計算修正量後
-                            delta_wt[i] = learning_rate * 
+                            //delta_wt[i] = learning_rate * 
+                                //(x_train[index_data][i] - wt[i]) * 
+                                //neightbor_func(
+                                    //pow(col-winner[1],2)+pow(row-winner[0],2),
+                                    //radius
+                                //);
+
+                            // 更新 weight
+                            wt[i] += learning_rate * 
                                 (x_train[index_data][i] - wt[i]) * 
                                 neightbor_func(
                                     pow(col-winner[1],2)+pow(row-winner[0],2),
                                     radius
                                 );
-                            // 更新 weight
-                            wt[i] += delta_wt[i];
                         }
-
-                        // 更新修正量 delta W
-                        topo[row][col].set_delta_w(delta_wt);
                         // 更新 Weight
                         topo[row][col].set_weight(wt);
                     }
@@ -184,28 +175,21 @@ class Topology2D {
             radius = radius * r_rate;
 
             // 若不想使用收斂作爲結束判定，則繼續下一輪
-            if (!converge) continue;
+            if (!converge) {
+                continue;
+            } else {
+                // 計算經歷一個 epoch 後，比對 training data 的準確率
+                cout << time << ":\n";
 
-            // 計算各單元經歷一個 epoch 後 weight 的誤差
-            keep = false;
-            for (int row = 0; row < topo.size(); row++) {
-                for (int col = 0; col < topo[row].size(); col++) {
-                    double error = 0;
-                    vector<double> wt = topo[row][col].get_weight();
+                keep = false;
+                double acc = test(x_train, y_train);
 
-                    for (int i = 0; i < wt.size(); i++) {
-                        error += sqrt(pow(wt[i] - pre_weight[row][col][i], 2));
-                    }
-
-                    // 判斷各單元 weight 的誤差是否過大
-                    if (error > 0.0000000001) {
-                        keep = true;
-                    }
+                if (acc < 100.0) {
+                    keep = true;
                 }
             }
         }
 
-        set_region(x_train, y_train);
         cout << "Done!\n\n";
     }
 
@@ -236,26 +220,20 @@ class Topology2D {
                 }
             }
 
-            //cout << "No." << index_data + 1
-                 //<<" winner is (" << winner[1] << "," << winner[0] << ") - Unit "
-                 //<< winner[0] * topo.size() + winner[1] + 1 << endl;
-
-            //cout << topo[winner[0]][winner[1]].get_output() << ":" << y_train[index_data] << endl;
+            // Debug 訊息：(該資料映射座標點) - 該點群集：該資料群集
             if (topo[winner[0]][winner[1]].get_output() == y_train[index_data]) {
                 acc++;
             } else {
                 if (y_train[index_data] == 0.0) {
-                    cout << topo[winner[0]][winner[1]].get_output() << " : Class 0\n";
+                    cout << "(" << winner[0] << ", " << winner[1] << ") - "
+                         << topo[winner[0]][winner[1]].get_output()
+                         << " : Class 0\n";
                 } else {
-                    cout << topo[winner[0]][winner[1]].get_output() << " : Class " << y_train[index_data] << endl;
+                    cout << "(" << winner[0] << ", " << winner[1] << ") - "
+                         << topo[winner[0]][winner[1]].get_output() << "："
+                         << y_train[index_data] << endl;
                 }
             }
-            
-            // 輸出原二維座標，並輸出映射到的單元
-            //char cls = 65;
-            //cls += winner[0] * topo.size() + winner[1];
-            //fout << x_train[index_data][1] << ',' << x_train[index_data][0] << ','
-                 //<< cls << endl;
             
             // 輸出映射後的座標
             fout << winner[1] << ',' << winner[0] << endl;
@@ -263,6 +241,7 @@ class Topology2D {
 
         fout.close();
         cout << "Accuracy: " << acc/x_train.size() * 100 << '%' << endl;
+        return acc/x_train.size() * 100;
     }
 
     void output_weight(void) {
@@ -270,10 +249,10 @@ class Topology2D {
         for (int row = 0; row < topo.size(); row++) {
             for (int col = 0; col < topo[row].size(); col++) {
                 vector<double> wt = topo[row][col].get_weight();
-                for (int i = 0; i < wt.size() - 1; i++) {
+                for (int i = 0; i < wt.size(); i++) {
                     fout << wt[i] << ',';
                 }
-                fout << wt[wt.size() - 1] << endl;
+                fout << topo[row][col].get_output() << endl;
             }
         }
         fout.close();
@@ -285,14 +264,14 @@ class Topology2D {
             for (int col = 0; col < topo[row].size(); col++) {
                 string str = "";
                 vector<double> wt = topo[row][col].get_weight();
-                for (int i = 0; i < wt.size() - 1; i++) {
+                for (int i = 0; i < wt.size(); i++) {
                     getline(fin, str, ',');
                     wt[i] = atof(str.c_str());
                     cout << wt[i] << ", ";  // for Debug
                 }
                 getline(fin, str, '\n');
-                wt[wt.size() - 1] = atof(str.c_str());
-                cout << wt[wt.size() - 1] << endl;  // for Debug
+                topo[row][col].set_output(atof(str.c_str()));
+                cout << str << endl;  // for Debug
 
                 topo[row][col].set_weight(wt);
             }
@@ -325,54 +304,8 @@ class Topology2D {
         fout.close();
     }
 
-    void set_region(vector<vector<double>> x_train, vector<double> y_train) {
-        // 初始化單元輸出
-        for (int row = 0; row < topo.size(); row++) {
-            for (int col = 0; col < topo[row].size(); col++) {
-                // 初始化當前單元輸出值爲 0.0
-                topo[row][col].set_output(0.0);
-            }
-        }
-
-        for (int index_data = 0; index_data < x_train.size(); index_data++) {
-            // 找出優勝單元 topo(i,j)
-            int winner[2] = {0, 0};    // 優勝單元拓撲座標
-            double min = 99999;        // 當前與 x_trian(i,j) 鄰近距離最小值
-            for (int row = 0; row < topo.size(); row++) {
-                for (int col = 0; col < topo[row].size(); col++) {
-                    vector<double> wt = topo[row][col].get_weight();
-
-                    // 計算鄰近距離
-                    double temp = 0;
-                    for (int i = 0; i < x_train[index_data].size(); i++) {
-                        temp += pow(x_train[index_data][i] - wt[i], 2);
-                    }
-
-                    // 若較當前最小值小，則更新優勝單元座標與當前最小值
-                    if (temp < min) {
-                        winner[0] = row;
-                        winner[1] = col;
-                        min = temp;
-                    }
-                }
-            }
-            
-            topo[winner[0]][winner[1]].set_output(y_train[index_data]);
-        }
-    }
-
   private:
     vector<vector<Unit>> topo;
-
-    //inline double vector_dis(Unit winner, Unit unit) {
-        //// distance 的平方 = 兩向量相減後的 norm 平方
-        //double temp = 0;
-        //vector<double> w = winner.get_weight(), u = unit.get_weight();
-        //for (int i = 0; i < w.size(); i++) {
-            //temp += pow(w[i] - u[i], 2);
-        //}
-        //return temp;
-    //}
 
     inline double neightbor_func(double dis, double radius) {
         // 以斗笠帽函數爲鄰近函數
@@ -390,18 +323,18 @@ void load_data_2D(
     string str;
 
     for (int i = 0; i < r; i++) {
-        cout << "資料 " << i << " 屬性: ";  // for Debug
+        //cout << "資料 " << i << " 屬性: ";  // for Debug
         for (int j = 0; j < c; j++) {
             getline(file, str, ',');
             data[i][j] = atof(str.c_str());
-            cout << data[i][j] << ", ";  // for Debug
+            //cout << data[i][j] << ", ";  // for Debug
         }
-        cout << endl;    // for Debug
+        //cout << endl;    // for Debug
 
-        cout << "標籤 " << i << " 屬性: ";  // for Debug
+        //cout << "標籤 " << i << " 屬性: ";  // for Debug
         getline(file, str, '\n');
         label[i] = atof(str.c_str());
-        cout << label[i]<< endl;  // for Debug
+        //cout << label[i]<< endl;  // for Debug
     }
 }
 
@@ -410,7 +343,7 @@ int main(int argc, char *argv[]) {
      * 指令：./執行檔名 1. 2. 3. 4.
      * 參數說明：
      * 1. 放在 training_data 底下的 training data 檔案名，需有副檔名 csv
-     * 2. training data 行數
+     * 2. training data 列數
      * 3. training data 屬性數
      * 附註：
      * 需改寫 testing data 行數變數
@@ -449,7 +382,7 @@ int main(int argc, char *argv[]) {
      * min: weight 隨機值的最小值
      * max: weight 隨機值的最大值
      */
-    Topology2D topo2D(9, 9, col, -1, 1);
+    Topology2D topo2D(10, 10, col, -1, 1);
     /*
      * x_train: training_data
      * epochs: 最多執行次數
@@ -459,7 +392,7 @@ int main(int argc, char *argv[]) {
      * lr_rate: 學習速率縮小率
      * converge: 使訓練到收斂爲止
      */
-    topo2D.train(training_data, label, 1000, 2, 0.8, 1, 0.5, false);
+    topo2D.train(training_data, label, 100, 5, 0.75, 2, 0.75, true);
 
     topo2D.output_weight();
     topo2D.output_region();
@@ -484,5 +417,6 @@ int main(int argc, char *argv[]) {
     /*
      * Testing
      */
+    topo2D.input_weight();
     topo2D.test(testing_data, label);
 }
